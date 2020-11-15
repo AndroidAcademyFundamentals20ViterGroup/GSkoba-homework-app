be9833702001db0e548bcd8381a2a049b4ba9792
Grigoriy Skobelev <gskobelevff@gmail.com>
2020-11-15 14:54:15 +0300

    HW-2 add MovieDetailsActivity

--- app/src/main/java/com/gskoba/wonderful/MainActivity.kt
+++ app/src/main/java/com/gskoba/wonderful/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.gskoba.wonderful
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setOnClickListener {
+            goToMovieDetailsActivity()
+        }
+    }
+
+    private fun goToMovieDetailsActivity() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
